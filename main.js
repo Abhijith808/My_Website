cb4b467436834f302480287eebfa27a4be95f4ba
Abhijith S <107806724+Abhijith808@users.noreply.github.com>
2023-08-25 16:39:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ const sr = ScrollReveal({
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, clock',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 
